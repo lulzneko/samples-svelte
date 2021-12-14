cf7d278070da9d07fd0b270ec954a8e42f8f8cf8
lulzneko <lulzneko@riotz.works>
2021-12-14 16:44:20 +0900

    feature: implement the top page

--- sveltekit-google-auth/src/routes/index.svelte
+++ sveltekit-google-auth/src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { signOut as authSignOut } from 'sk-auth/client';
+  import { session } from '$app/stores';
+
+  $: user = $session.user;
+
+  function signIn() {
+    location.assign('/api/auth/signin/google?redirect=/');
+  }
+
+  function signOut() {
+    authSignOut().then(session.set);
+  }
+</script>
+
+
+{#if !user}
+  <button on:click="{signIn}">サインイン with Google</button>
+{:else}
+  <h2>Hello {user.name} !!</h2>
+  <p><img src={user.picture} alt="{user.name} icon" /></p>
+  <button on:click={signOut}>サインアウト</button>
+{/if}
